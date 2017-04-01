30cb27fc28e6b321078500becfdd8bc111931b2c
Daksh Shah <dakshshah96@gmail.com>
2017-04-02 01:36:29 +0530

    Implement add and delete

--- app.js
+++ app.js
@@ -16,12 +16,30 @@ function addItem(state, item) {
     state.items.push(item);
 }
 
+function deleteItem(state, item) {
+    var index = state.items.indexOf(item);
+    if (index > -1) {
+        state.items.splice(index, 1);
+    }
+}
+
 function renderShoppingList(state, element) {
     var itemsHTML = state.items.map(function(item) {
-        return 0;
+        return '<li><span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
     });
+    element.html(itemsHTML);
 }
 
 $(function() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        addItem(state, $('#shopping-list-entry').val());
+        renderShoppingList(state, $('.shopping-list'));
+    });
 
+    $('#js-shopping-list-form').on('click', '.shopping-item-delete', function(event) {
+        event.preventDefault();
+        deleteItem(state, $(this).closest('.shopping-item').val());
+        renderShoppingList(state, $('.shopping-list'));
+    });
 });
